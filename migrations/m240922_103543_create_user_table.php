db03c5bbb73c494782a64c3f2eb3b173d66660d5
SKOLIA0 <skolia0@gmail.com>
2024-09-22 12:37:19 +0200

    Add migrations for user and note tables

---
+++ migrations/m240922_103543_create_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m240922_103543_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'password_hash' => $this->string()->notNull(),
+            'auth_key' => $this->string(32)->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+
+}
