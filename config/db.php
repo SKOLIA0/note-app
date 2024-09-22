63086b78a5d61696ce9429534843633bf390b230
SKOLIA0 <skolia0@gmail.com>
2024-09-22 12:35:22 +0200

    Configure database connection to Docker MySQL

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;port=3307;dbname=note_management_db',
+    'username' => 'user',
+    'password' => 'password',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
